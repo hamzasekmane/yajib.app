40512d84da53c09e40e59e94eca9e6ff330a6541
Hamza Sekmane <hamza02sekmane@gmail.com>
2026-09-07 23:22:15 +0100

    first commit

---
+++ drizzle.config.ts
@@ -0,0 +1,10 @@
+import "dotenv/config";
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  dialect: "postgresql",
+  schema: "./src/db/schema.ts",
+  dbCredentials: {
+    url: String(process.env.DATABASE_URL),
+  },
+});
