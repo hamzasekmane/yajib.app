417c12cab3fce1c817fc6853cc1224a3036342e7
sekmane hamza <155332650+hamzasekmane@users.noreply.github.com>
2026-09-07 23:33:11 +0100

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,14 @@
 import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not defined");
+}
+
 export default defineConfig({
   dialect: "postgresql",
   schema: "./src/db/schema.ts",
   dbCredentials: {
-    url: String(process.env.DATABASE_URL),
+    url: process.env.DATABASE_URL,
   },
 });
